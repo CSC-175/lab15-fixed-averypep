4b183f5b0516a864e4ec52b5cc64848f5dbc3868
Avery <156001719+averypep@users.noreply.github.com>
2024-03-20 12:15:24 -0400

    Update Lab15Functions.cpp

--- Lab15Functions.cpp
+++ Lab15Functions.cpp
@@ -1,45 +1,22 @@
 #include <iostream>
 using namespace std;
-/*******************************************************************
-* getInfo *
-* Gets and validates lottery info from the user and places it in   *
-* reference parameters referencing variables in the main function. *
-*******************************************************************/
+
+void getInfo(int & pickFrom, int & numPicks);
+double computeWays(int n, int k);
+double factorial(int n);
+
 void getInfo(int & pickFrom, int & numPicks){
-
-}
-
-/******************************************************************
-* computeWays                                                     *
-* Computes and returns the number of different possible sets      *
-* of k numbers that can be chosen from a set of n numbers.        *
-* The formula for this is     k!(n- k)!                           *
-*                             --------                            *
-*                                 n!                              *
-*******************************************************************/
-// Note that the computation is done in a way that does not require
-// multiplying two factorials together. This is done to prevent any
-// intermediate result becoming so large that it causes overflow.
-double computeWays(int n, int k){
-    
-}
-
-// This function computes factorials recursively. It is called by computeWays.
-double factorial(int n){
-
-int main(){
-  int pickFrom, numPicks;
-  getInfot(pickFrom, numPicks);
-  double ways = computeWays(pickFrom, numPicks);
-  cout << "Number of possible combinations: " << ways << endl;
-  return 0;
-}
-void getInfo(int & pickFrom, int & numPicks){
-  cout << "Enter the total of numbers to pick from: ";
+  cout << "How many balls (1-12) are in the pool to pick from? ";
   cin >> pickFrom;
-  cout << "Enter the number of numbers to pick: ";
+  cout << "How many balls (1-7) will be drawn? ";
   cin >> numPicks;
+  while (numPicks > pickFrom){
+    cout << "Input Error!";
+    cout << "\nHow many balls (1-7) will be drawn? ";
+    cin >> numPicks;
+  }
 }
+
 double computeWays(int n, int k){
   double ways = factorial(n) / (factorial(k) * factorial(n - k));
   return ways;
@@ -52,4 +29,9 @@ double factorial(int n){
     return n * factorial(n - 1);
   }
 }
+int main(){
+  int pickFrom, numPicks;
+  getInfo(pickFrom, numPicks);
+  double ways = computeWays(pickFrom, numPicks);
+  return 0;
 }
